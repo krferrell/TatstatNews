81293fd91dc5d70f61b52b699ea1fe48865d4b03
Kenneth Ferrell <kferrell@alphaworks.tech>
2022-01-25 15:41:40 -0800

    feat(app): create data cleaning function

--- tattoonews/client/src/App.js
+++ tattoonews/client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 var axios = require("axios").default;
 
 
@@ -16,21 +16,36 @@ function App() {
     axios
       .request(options)
       .then((response) => {
-        setNews(response.data.value[0].body);
+        // setNews(response.data.value);
+        console.log(response)
       })
       .catch((error) => {
         console.error(error);
       });
   };
 
+  // const cleanData = (newsArray) => {
+    
+  //   const filteredArray = newsArray?.filter((article)=>{
+  //   let result = article.body.includes("Link") || article.body.includes("Ruiz");
+  //   if(!result) return article;
+  //   });
+  //   console.log(filteredArray);
+  // };
+
+  useEffect(()=>{
+    getData();
+  }, [])
+
   return (
     <div>
-      <button onClick={getData}>Get Data</button>
-      <div>
-        {news}
-      </div>
+      <button
+      // onClick={() => cleanData(news)}
+      >
+        Click Me
+      </button>
     </div>
   );
-}
+  }
 
 export default App;
